63fd5a1529fe68c6efa52c5ab24efa0d3abbcbb0
TomWija <tomwi@brandwatch.com>
2020-08-02 18:54:58 +0100

    feat(mobile): add button to switch players on small screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./Player/index";
 
 import "./App.css";
 
 function App() {
+  const [visiblePlayer, setVisiblePlayer] = useState(true);
+
   return (
     <div className="App">
-      <Player id={1} />
-      <Player id={2} />
+      <button
+        className="App__switch-player-button"
+        onClick={() => setVisiblePlayer(!visiblePlayer)}>
+          Switch to Player { visiblePlayer ? 2 : 1 }
+      </button>
+      <div className="App__players">
+        <Player id={1} visible={ visiblePlayer }/>
+        <Player id={2} visible={ !visiblePlayer }/>
+      </div>
     </div>
   );
 }
--- src/Player/index.js
+++ src/Player/index.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import PlayerDetails from "../PlayerDetails";
 import PrimaryObjectiveScorer from "../PrimaryObjectiveScorer";
 import SecondaryObjectiveScorer from "../SecondaryObjectiveScorer";
 
 import "./style.css";
 
-const Player = ({id}) => {
+const Player = ({id, visible}) => {
   const [primaryPoints, setPrimaryPoints] = useState(0);
   const [secondaryPoints, setSecondaryPoints] = useState(0);
 
+  const classes = classNames("Player", {
+    "Player--visible": visible
+  });
+
   return (
-    <div className="Player">
+    <div className={ classes }>
       <PlayerDetails id={id} />
       <PrimaryObjectiveScorer updateTotal={setPrimaryPoints} />
       <SecondaryObjectiveScorer updateTotal={setSecondaryPoints} />
 
       <div className="total-points">
         <div className="total-points__label">
-                    Final Total
+          Final Total
         </div>
         <div className="total-points__number">
           { primaryPoints + secondaryPoints } / 90
@@ -28,7 +33,8 @@ const Player = ({id}) => {
   );
 };
 Player.propTypes = {
-  id: PropTypes.number.isRequired
+  id: PropTypes.number.isRequired,
+  visible: PropTypes.bool.isRequired
 };
 
 export default Player;
