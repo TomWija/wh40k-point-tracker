269d060036657a48cd5bcea394ce4fe4e5a18a24
TomWija <tomwi@brandwatch.com>
2020-08-02 17:14:50 +0100

    Updates

---
+++ precache-manifest.b1f04582fbf22a57afef142e306f3efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9f90f54eac9c7dc6ce24f307e3c683",
+    "url": "/wh40k-point-tracker/index.html"
+  },
+  {
+    "revision": "97ff9821425157d890e1",
+    "url": "/wh40k-point-tracker/static/css/main.98b7eb4b.chunk.css"
+  },
+  {
+    "revision": "ac64a2ab2d2aedf7f484",
+    "url": "/wh40k-point-tracker/static/js/2.329e051f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wh40k-point-tracker/static/js/2.329e051f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ff9821425157d890e1",
+    "url": "/wh40k-point-tracker/static/js/main.1fe855e1.chunk.js"
+  },
+  {
+    "revision": "339531b57f07c7e0c2b0",
+    "url": "/wh40k-point-tracker/static/js/runtime-main.4a24673b.js"
+  }
+]);
